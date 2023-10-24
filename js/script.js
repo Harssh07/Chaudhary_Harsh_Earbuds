3d12f416d33de178a144489e781570d9b17ac778
Harssh07 <121983623+Harssh07@users.noreply.github.com>
2023-10-24 01:20:38 -0400

    js added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,69 @@
-// Handles loading the events for <model-viewer>'s slotted progress bar
-const onProgress = (event) => {
-  const progressBar = event.target.querySelector('.progress-bar');
-  const updatingBar = event.target.querySelector('.update-bar');
-  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
-  if (event.detail.totalProgress === 1) {
-    progressBar.classList.add('hide');
-    event.target.removeEventListener('progress', onProgress);
-  } else {
-    progressBar.classList.remove('hide');
+(() => {
+  //console.log("IIFE Fired");
+  //variables
+  const model = document.querySelector("#model");
+  const hotspots = document.querySelectorAll(".Hotspot");
+
+  const InfoBoxes = [{
+       title: "Charging pot",
+       text: "Charge for 10 min and use for 10 hrs",
+       image: "../images/image_hotspot.svg"
+ }]
+
+  //functions
+  function modelLoaded() {
+    //console.log(hotspots);
+    hotspots.forEach(hotspot => {
+      hotspot.style.display = "block";
+    });
   }
-};
-document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+  function loadInfo() {
+    
+    InfoBoxes.forEach((infoBox, index)=>{
+      let selected = document.querySelector(`hotspot-${index + 1}`);
+      console.log(selected);
+      //document.createElement('h2'):
+      //textContent = infoBox.title
+
+
+      //document.createElement('p'):
+      //textContent = infoBox.text
+
+
+      // console.log(infoBox.title);
+      // console.log(infoBox.text);
+
+
+      // selected.appendChild();
+      // selected.appendChild();
+    })
+    //let selected = document.querySelector(`#hotspot-${a dynamic number}`)
+  }
+  loadInfo();
+
+  function showInfo() {
+    //console.log(this.slot);
+    //console.log(`#${this.slot}`);
+    //since the slot value matches the id value I can use the slot value as a selector to get to the div I want.
+    let selected = document.querySelector(`#${this.slot}`);
+    gsap.to(selected, 1, { autoAlpha: 1 });
+  }
+
+  function hideInfo() {
+    //console.log(this.slot);
+    //console.log(`#${this.slot}`);
+    let selected = document.querySelector(`#${this.slot}`);
+    gsap.to(selected, 1, { autoAlpha: 0 });
+  }
+
+  //Event Listener
+  model.addEventListener("load", modelLoaded);
+
+  hotspots.forEach(function (hotspot) {
+    hotspot.addEventListener("mouseover", showInfo);
+    hotspot.addEventListener("mouseout", hideInfo);
+  });
+})();
+
+// In this version, the event listeners use regular functions instead of arrow functions, so the "this" keyword inside the event listeners will refer to the DOM element that triggered the event.
