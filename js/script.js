fa0c1ecfa4d1f03c2ff8bcc4bf48cbb20dd13396
Harssh07 <121983623+Harssh07@users.noreply.github.com>
2023-10-25 09:20:39 -0400

    change in js

--- js/script.js
+++ js/script.js
@@ -1,65 +1,75 @@
 (() => {
-  // Variables
+  console.log("IIFE Fired");
+  //   variables
   const model = document.querySelector("#model");
   const hotspots = document.querySelectorAll(".Hotspot");
 
   const InfoBoxes = [
     {
-      title: "Charging pod",
-      text: "Charge for 10 min and use for 10 hrs",
-      image: "../images/ar_icon.png",
+      title: "led light logo",
+      text: "use the smart light indication in the back to know the battery percentage for the earbuds",
+      image: "../img/hotspot-svgrepo-com.svg",
     },
     {
-      title: "Volume up",
-      text: "Press once to increase volume and and press twice to receive incoming call",
-      image: "../images/ar_icon.png",
+      title: "magnetic charging",
+      text: "use the smart light indication in the back to know the battery percentage for the earbuds",
+      image: "../img/hotspot-svgrepo-com.svg",
     },
     {
-      title: "Volume Down",
-      text: "Press once to decrease volume",
-      image: "../images/ar_icon.png",
+      title: "comfortable ear peace",
+      text: "use the smart light indication in the back to know the battery percentage for the earbuds",
+      image: "../img/hotspot-svgrepo-com.svg",
     },
     {
-      title: "Sweat-free Eartips",
-      text: "Sweat free eartips for all usage",
-      image: "../images/ar_icon.png",
+      title: "light weight body",
+      text: "use the smart light indication in the back to know the battery percentage for the earbuds",
+      image: "../img/hotspot-svgrepo-com.svg",
     },
     {
-      title: "glossy finish body",
-      text: "Glossy finishing, which looks nice and smooth",
-      image: "../images/ar_icon.png",
+      title: "long battery life",
+      text: "use the smart light indication in the back to know the battery percentage for the earbuds",
+      image: "../img/hotspot-svgrepo-com.svg",
     },
   ];
+  // functions
 
-  // Functions
   function modelLoaded() {
-    hotspots.forEach(hotspot => {
-      hotspot.style.display = "block";
+    hotspots.forEach((hotspot) => {
+      hotspot.style.display = "inline";
     });
   }
 
+  function loadInfo() {
+    InfoBoxes.forEach((infoBox, index) => {
+      let selected = document.querySelector(`.hotspot-${index + 1}`);
+
+      const titleElement = document.createElement("h2");
+      titleElement.textContent = infoBox.title;
+
+      const textElement = document.createElement("p");
+      textElement.textContent = infoBox.text;
+
+      selected.appendChild(titleElement);
+      selected.appendChild(textElement);
+    });
+  }
+  loadInfo();
+
   function showInfo() {
-    let selected = document.querySelector(`#${this.getAttribute('slot')}`);
-    if (selected) {
-      gsap.to(selected, 1, { autoAlpha: 1 });
-    }
+    let selected = document.querySelector(`#${this.slot}`);
+    gsap.to(selected, 1, { autoAlpha: 1 });
   }
 
   function hideInfo() {
-    let selected = document.querySelector(`#${this.getAttribute('slot')}`);
-    if (selected) {
-      gsap.to(selected, 1, { autoAlpha: 0 });
-    }
+    let selected = document.querySelector(`#${this.slot}`);
+    gsap.to(selected, 3, { autoAlpha: 0 });
   }
 
-  // Event Listener
-  if (model) {
-    model.addEventListener("load", modelLoaded);
-  }
+  // event listeners
+  model.addEventListener("load", modelLoaded);
 
   hotspots.forEach(function (hotspot) {
-    hotspot.addEventListener("mouseover", showInfo);
+    hotspot.addEventListener("mousehover", showInfo);
     hotspot.addEventListener("mouseout", hideInfo);
   });
-
 })();
